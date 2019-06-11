237bc6483cd2708b1d15a012b634f099f8316638
melodybs <melody_bs@naver.com>
2019-06-12 00:09:51 +0900

    190612

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,44 @@
 <template>
+  <ErrorBoundary id="app">
+    <router-view />
+  </ErrorBoundary>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component({ name: 'App' })
+export default class App extends Vue {}
+</script>
+<!--
+<script>
+export default {
+  name: 'App'
+}
+</script>
+-->
+
+<style lang="less">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+<!--
+<template>
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
         <span>Vuetify</span>
         <span class="font-weight-light">MATERIAL DESIGN</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
+      <v-spacer />
       <v-btn
         flat
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
@@ -16,7 +49,7 @@
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <HelloWorld />
     </v-content>
   </v-app>
 </template>
@@ -36,3 +69,4 @@ export default {
   }
 }
 </script>
+-->
